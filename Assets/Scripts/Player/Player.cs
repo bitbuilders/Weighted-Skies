43c5608809866dd0be4fa6a82a88a5b112fc1bcf
Colin Misbach <colin@misbach.com>
2018-04-05 21:59:12 -0600

    Fixed up character model animator | Improved player prefab

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -31,6 +31,22 @@ public class Player : MonoBehaviour
             Vector3 force = Vector3.up * m_jumpForce;
             m_rigidbody.AddForce(force, ForceMode.Impulse);
         }
+        else if (Input.GetButtonDown("Jab"))
+        {
+            m_animator.SetTrigger("Jab");
+        }
+        else if (Input.GetButtonDown("Heavy Slice"))
+        {
+            m_animator.SetTrigger("Windmill");
+        }
+        else if (Input.GetButtonDown("Slam"))
+        {
+            m_animator.SetTrigger("Slam");
+        }
+        else if (Input.GetButtonDown("Ability"))
+        {
+            m_animator.SetTrigger("Ability");
+        }
     }
 
     private void FixedUpdate()
@@ -43,12 +59,12 @@ public class Player : MonoBehaviour
         velocity.z = Input.GetAxis("Vertical");
         velocity = velocity * m_speed * 100.0f * Time.deltaTime;
         m_rigidbody.AddRelativeForce(velocity, ForceMode.Force);
-
-        if (m_rigidbody.velocity.y > 0.0f)
+        
+        if (m_rigidbody.velocity.y > 0.1f)
         {
             m_rigidbody.velocity += (Vector3.up * Physics.gravity.y) * (m_jumpResistance - 1.0f) * Time.deltaTime;
         }
-        else if (m_rigidbody.velocity.y < 0.0f)
+        else if (m_rigidbody.velocity.y < -0.1f)
         {
             m_rigidbody.velocity += (Vector3.up * Physics.gravity.y) * (m_fallMultiplier - 1.0f) * Time.deltaTime;
         }
@@ -57,7 +73,19 @@ public class Player : MonoBehaviour
 
         Collider[] points = Physics.OverlapSphere(m_groundTouchPoint.position, 0.2f, m_groundMask);
         m_onGround = points.Length > 0;
-        m_onGround = true;
         m_animator.SetBool("OnGround", OnGround);
+
+        Vector3 speed = new Vector3(m_rigidbody.velocity.x, 0.0f, m_rigidbody.velocity.z);
+        float walkSpeed = speed.magnitude;
+        float runSpeed = walkSpeed - 6.0f;
+        if (walkSpeed <= 0.01f) walkSpeed = 0.0f;
+
+        float animationSpeedWalk = walkSpeed * 0.5f;
+        float animationSpeedRun = walkSpeed * 0.13f;
+
+        m_animator.SetFloat("Walking", walkSpeed);
+        m_animator.SetFloat("Running", runSpeed);
+        m_animator.SetFloat("WalkSpeed", animationSpeedWalk);
+        m_animator.SetFloat("RunSpeed", animationSpeedRun);
     }
 }
